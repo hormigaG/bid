c36aefe98f042283b91e22afa76ec6b7e5014958
aaejandroabele <alejandroabele@hotmail.com.ar>
2022-03-16 19:27:36 -0300

    [ADD] Mode fullscreen

--- src/app/main/menu/menu.component.ts
+++ src/app/main/menu/menu.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.css']
+  styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  fullScreen() {
+    let elem = document.documentElement;
+    let methodToBeInvoked =
+      elem.requestFullscreen ||
+      elem['mozRequestFullscreen'] ||
+      elem['msRequestFullscreen'];
+    if (methodToBeInvoked) methodToBeInvoked.call(elem);
   }
 
 }
